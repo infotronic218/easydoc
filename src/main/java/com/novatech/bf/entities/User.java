5d7947b3afb542bce89ff2f0d2183e8d04202539
infotronic218 <infotronic218@gmail.com>
2018-10-27 20:20:16 +0100

    mmm

--- src/main/java/com/novatech/bf/entities/User.java
+++ src/main/java/com/novatech/bf/entities/User.java
@@ -6,9 +6,11 @@ import java.util.Collection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.Length;
 @Entity
+@Table(name="eusers")
 public abstract class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
